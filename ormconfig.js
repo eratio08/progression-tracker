ec491a5458398744554dfb9adeebf4396a46ecde
Eike Lurz <eike.lurz@tuta.io>
2019-08-17 19:21:22 +0200

    impr: enchanges ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+const envalid = require("envalid");
+
+const { TYPEORM_USERNAME, TYPEORM_PASSWORD } = envalid.cleanEnv(process.env, {
+  TYPEORM_USERNAME: envalid.str(),
+  TYPEORM_PASSWORD: envalid.str()
+});
+
+module.exports = {
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "dev",
+  username: TYPEORM_USERNAME,
+  password: TYPEORM_PASSWORD,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entities/**/*.ts"],
+  migrations: ["src/migrations/**/*.ts"],
+  subscribers: ["src/subscribers/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entities",
+    migrationsDir: "src/migrations",
+    subscribersDir: "src/subscribers"
+  }
+};
